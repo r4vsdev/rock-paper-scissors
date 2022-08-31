5cb6e1850ce34ea10097e4c1e8ced829c7f09509
r4vsdev <r4vsdev@gmail.com>
2022-08-30 23:02:42 -0300

    Improve the UI

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let playerSelection;
 let result;
+let computerScore = 0;
+let playerScore   = 0;
 
 function getComputerChoice () {
     let x = Math.floor(Math.random() * 3);
@@ -12,12 +14,15 @@ function getComputerChoice () {
     }
 }
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.result-container');
 
 const content = document.createElement('div');
 content.textContent = result;
-
 container.appendChild(content);
+
+// const content2 = document.createElement('div');
+// content2.textContent = 'Score Board:';
+// container.appendChild(content2);
 
 const btnRock = document.querySelector('#rock');
 btnRock.addEventListener('click', () => {
@@ -44,6 +49,8 @@ btnScissors.addEventListener('click', () => {
 });
 
 function playRound (playerSelection) {
+    if (playerScore === 5 || computerScore === 5) {return }
+
     const computerSelection = getComputerChoice();
     // console.log('computer selection =',computerSelection);
     // console.log(`${playerSelection} vs ${computerSelection}`);
@@ -58,19 +65,29 @@ function playRound (playerSelection) {
     
     // Lose
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore += 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore += 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore += 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     
     // Won
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore += 1;
         return `You Won! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore += 1;
         return `You Won! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore += 1;
         return `You Won! ${playerSelection} beats ${computerSelection}`;
     }
+    
+}
+
+function playGame (playerSelection) {
     
 }
