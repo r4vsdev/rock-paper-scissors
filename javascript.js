6d5e55868541b689ecf80545eb3e5243fd0fa0e9
r4vsdev <r4vsdev@gmail.com>
2022-09-02 00:24:44 -0300

    Set header, main and footer basic structure and colors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+
+// TO DO:
+// separa a coloracao entre o titulo, o local dos botoes, e o fim da pagina onde o resultado final vai aparecer;
+// coloca um rodape com meu nome r4vsdev;
+// substitui os botoes do player por icones do whatsapp;
+// acrescenta os mesmos icones do lado do cpu;
+// centraliza o placar
+// o resultado final vai embaixo
+
+
 let playerSelection;
 let computerSelection;
 let matchResult;
